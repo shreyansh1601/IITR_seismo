9cd5065685b86120bb1a41b21f3836593ddea9c5
Vipul Silwal <vipulsilwal@gmail.com>
2020-01-20 09:51:53 +0530

    Fixed d_exact estimation; more comments

--- classes/ES422/VSP.m
+++ classes/ES422/VSP.m
@@ -1,46 +1,69 @@
-% Problem Set1 : Question 3
+% Problem Set1
+% Parameter Estimation and Inverse Problems
+% Author: Aster, Brocher, and Thurber
+% Third Edition (2019)
+% 
+% Chapter 1
+% Section 1.6: Question 3
+% 
+% Vertical Seismic Profile Example
+% 
 % Vipul Silwal
 
 clear all
 close all
 
+%==== Input Parameters =======
+% Number of sensors (Unitless)
+%N = 10;
 N = 100;
-%N = 4;
+%N = 1000;
 
+% Depth of first and last sensor (in meters)
 z_inital = 0;
 z_final = 20;
 
-% discretizing the depth space
+% velocity gradient (v = v0 + kz)
+k = 100;    % in m/s/m 
+v0 = 1000; % in m/s
+%=============================
+
+% discretizing the depth vector
 z_points = linspace(z_inital,z_final,N+1);
 sensor_depth = z_points(2:length(z_points));
 
+% sensor spacing in depth
+dz = z_points(2) - z_points(1);
+
 % creating mid-points for each layer
-for i=1:length(z_points)-1
-    z_mid(i) = z_points(i)+(z_points(i+1) - z_points(i))/2;
+for i = 1:N
+    z_mid(i) = z_points(i) + dz/2;
 end
 
 % making design matrix
 G_base = tril(ones(N,N));
-G = XXX
+G = G_base * dz;
 
 % making model_true space
-for i=1:length(z_points)-1
-    m_true(i,1) = XXX
-end
+% When our sensor spacing (and hence dz tends to 0) this should be close to
+% the d_exact
+m_true = 1./(v0 + k*z_mid);
+m_true = m_true'; 
 
 % calculating data_true
-d_true = G*m_true;
+d_true = G * m_true;
 
 % evaluating data_exact
-for i=1:length(z_points)-1
-    temp = 0.2/((8*i)+996);
-    if i>1
-        d_exact(i) = temp + d_exact(i-1);
-    else
-        d_exact(i) = temp;
-    end
-end
+% exact data can be obtained by analytical solving the integral equation
+% Solve the following definate integral
+% d(z)  = integral_0_to_z (1/(v0 + ke)) de
+d_exact = XXX;
 d_exact = d_exact';
+
+% TASKS: 
+% 1. Check the difference between d_true and d_exact
+% 2. Confirm that the difference decreases as number of sensors (N)
+% increases
 
 % plotting
 figure;
@@ -51,32 +74,39 @@ plot(z_points(2:length(z_points)),d_exact,'or');
 legend('d\_true','d\_exact');
 xlabel('z (sensor depth)');
 ylabel('data\_space (travel time)');
-title('Data sapce');
+title('Data space');
 grid on;
 
 %======================Inversion=====================
-
 % solving for m
-m = XXX;
+m_exact = G\d_exact;
+
+% TASKS: 
+% 1. Check the difference between m_true and m_exact
 
 % plotting
 subplot(2,1,2);
 hold on
 plot(z_mid,m_true,'xb');
-plot(z_mid,m,'or');
-legend('m\_true','m\_inv');
+plot(z_mid,m_exact,'or');
+legend('m\_true','m\_exact');
 ylabel('model\_parameter (slowness)');
 xlabel('z (interval midpoint)');
-title('Model sapce');
+title('Model space');
 grid on;
 
 %===================Adding noise=====================
-
-std = .05;
+std = .000005; % in seconds
 error = (std)*randn(N,1);
-d_obs = d_exact+error;
-m_inv = XXX;
+d_obs = d_exact + error;
+m_inv = G\d_obs;
 figure
+
+% TASKS: 
+% 1. Check the difference between m_true and m_inv after varying the number
+% of sensors
+% 2. Check the condition number of matrix G and confirm that it is the
+% culprit
 
 % plotting
 subplot(2,1,1);
@@ -86,32 +116,34 @@ plot(z_points(2:length(z_points)),d_obs,'or');
 legend('d\_true','d\_exact+noise');
 xlabel('z (sensor depth)');
 ylabel('data\_space (travel time)');
-title('Data sapce + noise');
+title('Data space + noise');
 grid on;
 
 subplot(2,1,2);
 hold on
 plot(z_mid,m_true,'xb');
-plot(z_mid,m_inv,'or');
+plot(z_mid,m_inv,'or'); 
 legend('m\_true','m\_inv');
 ylabel('model\_parameter (slowness)');
 xlabel('z (interval midpoint)');
 title('Inversion after adding noise to data');
 grid on;
 
-%============additional figures=====================
-if (0)
-    x=-20:.01:20;
-    
-    plot(x,[0],'-');
+% ================ additional figures ==================
+% Propagating wavefront
+if (1)
+    figure
     hold on
-    z = -20:.01:0;
-    plot([0],z,'-');
     
-    for i = 1:length(z)/100
-        r=z(100*i);
-        y=sqrt(r^2-x.^2);
-        Y=real(y);
-        plot(x,-Y);
+    for i = 1:10:N
+    x = v0 * d_exact(i);
+    y = sensor_depth(i);
+    
+    [x1,y1] = ellipse(x,y,pi/1,0,0,'r');
+    idx = find (y1>0);
+    y1(idx) = 0;
+    plot(x1,y1,'r-', 'LineWidth', 3)
     end
+    plot([0 0],[0 -20], 'k-', 'LineWidth', 10);  
+    axis equal
 end
